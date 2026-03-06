97243586142f1e70ee223c1431e4bc495c61f1cf
Manish0085 <manishk57107@gmail.com>
2026-03-06 12:07:46 +0530

    Change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://videly.duckdns.org',
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_BASE_URL || 'https://videly.duckdns.org',
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
+  }
 })
